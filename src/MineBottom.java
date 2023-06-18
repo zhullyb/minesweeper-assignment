ecdecf4f9898e33b38b06156a111dc8ae4dd7457
zhullyb <zhullyb@outlook.com>
2023-06-18 19:37:00 +0800

    Resolve the problem that the mines can be duplicated

--- src/MineBottom.java
+++ src/MineBottom.java
@@ -3,12 +3,20 @@ package src;
 public class MineBottom {
     int[] mines = new int[GameUtil.MINE_NUM * 2];
     int x,y;
+    boolean isPlace = true;
     {
         for(int i = 0; i < GameUtil.MINE_NUM; i++){
             x = (int) (Math.random() * GameUtil.MAP_W + 1);
             y = (int) (Math.random() * GameUtil.MAP_H + 1);
             mines[i*2] = x;
             mines[i*2+1] = y;
+
+            for(int j = 0; j < i; j++){
+                if(mines[j*2] == x && mines[j*2+1] == y){
+                    i--;
+                    break;
+                }
+            }
         }
 
         for(int i=0; i < GameUtil.MINE_NUM; i++){
